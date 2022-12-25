1f3e2f119895be64bbc07afa714cd4ced899716c
Tonya <elzeresdai@gmail.com>
2022-12-25 14:24:39 +0200

    created functions for checking data and returning error with wrong index or result

--- internal/models.go
+++ internal/models.go
@@ -1,11 +1,5 @@
 package internal
 
 type requestStruct struct {
-	Numbers []int `json:"numbers"`
-}
-
-type errorStruct struct {
-	code    int
-	index   string
-	message string
+	Numbers []any `json:"numbers"`
 }
